e4257bebb67d3798b28e18eb1c58dd67c67799f1
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-12 12:22:36 +0530

    Add user-related routes for password change, profile retrieval, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { logOutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  logOutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentUserPassword,
+  getCurrentUser,
+  updateUserdetails,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getUserWatchHisteory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { loginUser } from "../controllers/user.controller.js";
-import {verifyjwt} from '../middleware/auth.middleware.js'
+import { verifyjwt } from "../middleware/auth.middleware.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
@@ -20,7 +31,44 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyjwt, logOutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router
+ .route("/login")
+ .post(loginUser);
+
+router
+ .route("/logout")
+ .post(verifyjwt, logOutUser);
+
+router
+ .route("/refresh-token")
+ .post(refreshAccessToken);
+
+router
+ .route("/change-password")
+ .post(verifyjwt, changeCurrentUserPassword);
+
+router
+ .route("/getCurrengUser-details")
+ .get(verifyjwt, getCurrentUser);
+
+router
+ .route("/updateUser-details")
+ .patch(verifyjwt, updateUserdetails);
+
+router
+  .route("/avatar")
+  .patch(verifyjwt, upload.single("avatar"), updateUserdetails);
+
+router
+  .route("/coverImage")
+  .patch(verifyjwt, upload.single("coverImage"), updateUserCoverImage);
+
+router
+ .route("/c/:username")
+.get(verifyjwt, getUserChannelProfile);
+ 
+router
+.route("/history")
+.get(verifyjwt, getUserWatchHisteory);
+
 export default router;
